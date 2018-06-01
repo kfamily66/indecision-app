05b5ca5884283527a0fc5770283558b6afd1e321
unknown <kfamily66@yandex.ru>
2018-06-01 21:26:19 +0500

    Toggle-visibility example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,87 +1,36 @@
 "use strict";
 
-var app = {
-  title: "Indecision app",
-  subtitle: "Put your life in the hands of a computer",
-  options: []
-};
+var visibility = false;
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = "";
-    renderApp();
-  }
-};
-
-var removeAll = function removeAll() {
-  app.options = [];
-  renderApp();
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  alert(app.options[randomNum]);
+var toggleDetails = function toggleDetails() {
+  visibility = !visibility;
+  render();
 };
 
 var appRoot = document.getElementById("app");
 
-var renderApp = function renderApp() {
+var render = function render() {
   var template = React.createElement(
     "div",
     null,
     React.createElement(
       "h1",
       null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      "p",
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? "Here your options" : "No options"
+      "Toggle visibility"
     ),
     React.createElement(
       "button",
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      "What should I do?"
+      { onClick: toggleDetails },
+      visibility ? "Hide details" : "Show details"
     ),
-    React.createElement(
-      "ol",
+    visibility && React.createElement(
+      "p",
       null,
-      app.options.map(function (option) {
-        return React.createElement(
-          "li",
-          { key: option },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { autoFocus: true, type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add option"
-      ),
-      React.createElement(
-        "button",
-        { onClick: removeAll },
-        "Remove All"
-      )
+      "Here some additional info about it!"
     )
   );
 
   ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+render();
