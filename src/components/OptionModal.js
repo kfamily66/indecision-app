f45267afb13d9c24cb57fd3e244603998724a3f5
unknown <kfamily66@yandex.ru>
2018-07-08 23:09:04 +0500

    Add svg background and drop shadow

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -10,7 +10,7 @@ const OptionModal = props => (
     closeTimeoutMS={200}
     ariaHideApp={false}
   >
-    <h3 className="modal__title">You should choice</h3>
+    <h3 className="modal__title">You should choose</h3>
     {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
     <button className="button" onClick={props.handleClearSelectedOption}>
       Okay
